e2d6ed6d9ea5c813cfc8e91a41c1476d639c48b2
Debi Praharadika <blacknaml.laits@gmail.com>
2011-10-17 22:26:57 +0700

    lanjutkan ...

--- main.cpp
+++ main.cpp
@@ -23,9 +23,30 @@ const QString &Mahasiswa::nim() const {
     return _nim;
 }
 
+void Mahasiswa::setNim(const QString &nim){
+    _nim = nim;
+}
+
+int Mahasiswa::getNimLength() const {
+    return _nim.length();
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QObject parent;
+    Mahasiswa *objMhs1, *objMhs2, *objMhs3;
+
+    objMhs1 = new Mahasiswa("11223344",&parent);
+    objMhs2 = new Mahasiswa("22334455666",&parent);
+    objMhs3 = new Mahasiswa("11223334444", &parent);
+
+    qDebug() << objMhs1->nim() << " : " << objMhs1->getNimLength() << " kar";
+    objMhs1->setNim(objMhs2->nim());
+    objMhs2->setNim(objMhs3->nim());
+
+    qDebug() << objMhs1->nim() << " : " << objMhs1->getNimLength() << " kar";
+    qDebug() << objMhs1->nim() << " : " << objMhs1->getNimLength() << " kar";
     return a.exec();
 }
